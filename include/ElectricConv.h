efd8dfe26196c53ecc002341c8cc074906a2a023
luv4bytes <61706439+luv4bytes@users.noreply.github.com>
2020-09-11 10:41:14 +0200

    Copyright notice

--- include/ElectricConv.h
+++ include/ElectricConv.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2020 Lukas Pfeifer
+ * for ElectricConv version 1.0
+ *      https://github.com/luv4bytes/ElectricConv
+ *
+ * This file is part of ElectricConv.
+ *
+ * ElectricConv is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ElectricConv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with ElectricConv. If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
 #ifndef CONVERTER_H
 #define CONVERTER_H
 
